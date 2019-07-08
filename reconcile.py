3380eb326dc566c489ca3ecb292f777be2860c4f
KDMortimer <43451442+KDMortimer@users.noreply.github.com>
2019-07-08 12:59:15 -0300

    Move to SRUSearch
    I think I'm missing something w.r.t. querytypes. Not sure if I'm doing them right.

--- reconcile.py
+++ reconcile.py
@@ -7,6 +7,11 @@ http://www.oclc.org/developer/documentation/fast-linked-data-api/request-types
 
 This code is adapted from Michael Stephens:
 https://github.com/mikejs/reconcile-demo
+
+Further adapted from CMHarlow and merging in ideas from my SRUSearch webapp
+https://github.com/cmharlow/fast-reconcile for CMHarlow
+https://kdmortimer.github.io/FASTSRUtest.html for the SRUSearch app
+
 """
 
 from flask import Flask
@@ -14,8 +19,12 @@ from flask import request
 from flask import jsonify
 
 import json
+from json import dumps
+from collections import OrderedDict
+from xmljson import BadgerFish
 from operator import itemgetter
-import urllib
+
+from xml.etree.ElementTree import fromstring
 from sys import version_info
 
 #For scoring results
@@ -23,9 +32,9 @@ from fuzzywuzzy import fuzz
 import requests
 
 app = Flask(__name__)
-
+bf = BadgerFish(dict_type=OrderedDict) 
 #some config
-api_base_url = 'http://fast.oclc.org/searchfast/fastsuggest'
+api_base_url = 'http://experimental.worldcat.org/fast/search'
 #For constructing links to FAST.
 fast_uri_base = 'http://id.worldcat.org/fast/{0}'
 
@@ -41,51 +50,54 @@ except ImportError:
     app.logger.debug("No request cache found.")
     pass
 
-#Helper text processing
-import text
 
 #Map the FAST query indexes to service types
 default_query = {
-    "id": "/fast/all",
-    "name": "All FAST terms",
-    "index": "suggestall"
+    "id": "/SRUfast/all",
+    "name": "All FAST headings",
+    "index": "cql.any"
 }
 
 refine_to_fast = [
     {
-        "id": "/fast/geographic",
-        "name": "Geographic Name",
-        "index": "suggest51"
+        "id": "/SRUfast/topic",
+        "name": "Topical headings",
+        "index": "oclc.topic"
     },
     {
-        "id": "/fast/corporate-name",
-        "name": "Corporate Name",
-        "index": "suggest10"
+        "id": "/SRUfast/geographic",
+        "name": "Geographical headings",
+        "index": "oclc.geographic"
     },
     {
-        "id": "/fast/personal-name",
-        "name": "Personal Name",
-        "index": "suggest00"
+        "id": "/SRUfast/event",
+        "name": "Event headings",
+        "index": "oclc.eventName"
     },
     {
-        "id": "/fast/event",
-        "name": "Event",
-        "index": "suggest11"
+        "id": "/SRUfast/title",
+        "name": "Uniform title headings",
+        "index": "oclc.uniformTitle"
     },
     {
-        "id": "/fast/title",
-        "name": "Uniform Title",
-        "index": "suggest30"
+        "id": "/SRUfast/corporate",
+        "name": "Corporate name headings",
+        "index": "oclc.corporateName"
     },
     {
-        "id": "/fast/topical",
-        "name": "Topical",
-        "index": "suggest50"
+        "id": "/SRUfast/form",
+        "name": "Form headings",
+        "index": "oclc.form"
     },
     {
-        "id": "/fast/form",
-        "name": "Form",
-        "index": "suggest55"
+        "id": "/SRUfast/period",
+        "name": "Period headings",
+        "index": "oclc.period"
+    },
+    {
+        "id": "/SRUfast/LoC",
+        "name": "Library of Congress source headings",
+        "index": "oclc.altlc"
     }
 ]
 refine_to_fast.append(default_query)
@@ -128,64 +140,73 @@ def jsonpify(obj):
         return jsonify(obj)
 
 
-def search(raw_query, query_type='/fast/all'):
+def search(raw_query, query_type='/SRUfast/all', numRecords = 20, startRecord = 1, returnNum = 20, sortedResults = False):
     """
     Hit the FAST API for names.
     """
     out = []
     unique_fast_ids = []
-    query = text.normalize(raw_query, PY3).replace('the university of', 'university of').strip()
     query_type_meta = [i for i in refine_to_fast if i['id'] == query_type]
     if query_type_meta == []:
         query_type_meta = default_query
     query_index = query_type_meta[0]['index']
     try:
         #FAST api requires spaces to be encoded as %20 rather than +
-        url = api_base_url + '?query=' + urllib.quote(query)
-        url += '&rows=30&queryReturn=suggestall%2Cidroot%2Cauth%2cscore&suggest=autoSubject'
-        url += '&queryIndex=' + query_index + '&wt=json'
-        app.logger.debug("FAST API url is " + url)
+        url = api_base_url + "?query=" + query_index +'+all+"' + raw_query + '"&httpAccept=application/xml&maximumRecords=' 
+        url += str(numRecords) + '&startRecord=' + str(startRecord) +'&sortKey=usage&recordSchema=info:srw/schema/1/rdf-v2.0'
+        #app.logger.debug("FAST API url is " + url)
         resp = requests.get(url)
-        results = resp.json()
-    except Exception, e:
+        jsonResp = dumps(bf.data(fromstring(resp.content)))
+        #print(jsonResp)
+        results = json.loads(jsonResp)
+        #print(results)
+    except Exception as e:
         app.logger.warning(e)
         return out
-    for position, item in enumerate(results['response']['docs']):
-        match = False
-        name = item.get('auth')
-        alternate = item.get('suggestall')
-        if (len(alternate) > 0):
-            alt = alternate[0]
-        else:
-            alt = ''
-        fid = item.get('idroot')
+    for record in (results["{http://www.loc.gov/zing/srw/}searchRetrieveResponse"][ "{http://www.loc.gov/zing/srw/}records"]['{http://www.loc.gov/zing/srw/}record']):
+        minRecord = record["{http://www.loc.gov/zing/srw/}recordData"]["{http://www.w3.org/1999/02/22-rdf-syntax-ns#}RDF"]["{http://www.w3.org/1999/02/22-rdf-syntax-ns#}Description"][0]
+        #print(minRecord)
+        name = minRecord['{http://www.w3.org/2004/02/skos/core#}prefLabel']['$']
+        alt = []
+        if '{http://www.w3.org/2004/02/skos/core#}altLabel' in minRecord:
+            #print(minRecord['{http://www.w3.org/2004/02/skos/core#}altLabel'])
+            for altLabel in minRecord['{http://www.w3.org/2004/02/skos/core#}altLabel']:
+                alt.append(altLabel)            
+        print(alt)
+        fid = str(minRecord['{http://purl.org/dc/terms/}identifier']['$'])
         fast_uri = make_uri(fid)
         #The FAST service returns many duplicates.  Avoid returning many of the
         #same result
+        #I don't think this does anything in the SRU search
         if fid in unique_fast_ids:
             continue
         else:
             unique_fast_ids.append(fid)
-        score_1 = fuzz.token_sort_ratio(query, name)
-        score_2 = fuzz.token_sort_ratio(query, alt)
+        score_1 = fuzz.token_sort_ratio(raw_query, name)
+        scoreBundle = []
+        for altLabel in alt:
+            scoreBundle.append(fuzz.token_sort_ratio(raw_query, altLabel))
+        if scoreBundle:
+            score_2 = max(scoreBundle)
+        else:
+            score_2 = 0
         #Return a maximum score
         score = max(score_1, score_2)
-        if query == text.normalize(name, PY3):
-            match = True
-        elif query == text.normalize(alt, PY3):
-            match = True
+
         resource = {
             "id": fast_uri,
             "name": name,
             "score": score,
-            "match": match,
-            "type": query_type_meta
+            "type": query_type_meta[0]
         }
         out.append(resource)
     #Sort this list by score
     sorted_out = sorted(out, key=itemgetter('score'), reverse=True)
     #Refine only will handle top three matches.
-    return sorted_out[:3]
+    if sortedResults == True:
+        return sorted_out[:returnNum]
+    else:
+        return out[:returnNum]
 
 
 @app.route("/reconcile", methods=['POST', 'GET'])
@@ -196,14 +217,14 @@ def reconcile():
     if query is None:
         #Then normal get param.s
         query = request.args.get('query')
-        query_type = request.args.get('type', '/fast/all')
+        query_type = request.args.get('type', '/SRUfast/all')
     if query:
         # If the 'query' param starts with a "{" then it is a JSON object
         # with the search string as the 'query' member. Otherwise,
         # the 'query' param is the search string itself.
         if query.startswith("{"):
             query = json.loads(query)['query']
-        results = search(query, query_type=query_type)
+        results = search(query, query_type=query_type) #what is this supposed to do?
         return jsonpify({"result": results})
     # If a 'queries' parameter is supplied then it is a dictionary
     # of (key, query) pairs representing a batch of queries. We
